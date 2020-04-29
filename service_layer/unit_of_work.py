a64bec7709e3aac981eecff072a68e8d9d80b87a
joachim.hagege <joachim.hagege@alibaba-inc.com>
2020-04-29 21:53:22 +0300

    Defining interface for unit of work (according to integ test)

---
+++ service_layer/unit_of_work.py
@@ -0,0 +1,28 @@
+# pylint: disable=attribute-defined-outside-init
+from __future__ import annotations
+import abc
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+import config
+from adapters import repository
+
+
+class AbstractUnitOfWork(abc.ABC):
+    batches: repository.AbstractRepository
+
+    def __enter__(self) -> AbstractUnitOfWork:
+        return self
+
+    def __exit__(self, *args):
+        self.rollback()
+
+    @abc.abstractmethod
+    def commit(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def rollback(self):
+        raise NotImplementedError
+
